5a4ef3346b8b5b892ebb236f98a714ab3cc28a1f
Monica Vera <MonicaVera2002@github.com>
2025-12-08 07:53:24 -0600

    ajustes

---
+++ app/Http/Controllers/OperationsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class OperationsController extends Controller
+{
+    public function addition(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+
+    /**
+     * @return array{imc: float, categoria: string}
+     */
+    public function calcularIMC(float $peso, float $altura): array
+    {
+        if ($peso <= 0 || $altura <= 0) {
+            throw new \InvalidArgumentException('La altura y el peso deben ser mayor que cero.');
+        }
+
+        $imc = $peso / ($altura * $altura);
+        $imc = round($imc, 2);
+
+        if ($imc < 18.5) {
+            $categoria = 'Bajo peso';
+        } elseif ($imc < 25) {
+            $categoria = 'Normal';
+        } elseif ($imc < 30) {
+            $categoria = 'Sobrepeso';
+        } else {
+            $categoria = 'Obesidad';
+        }
+
+        return [
+            'imc' => $imc,
+            'categoria' => $categoria,
+        ];
+    }
+}
